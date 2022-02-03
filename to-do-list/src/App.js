ccecad06a359c28e5097ebe46df9e253e4eeaa0c
soleu <dlthf555@gmail.com>
2022-02-03 23:47:55 +0900

    add : todolist 구현중

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return; //비어있다면 실행하지않게함
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    console.log(toDos);
+    setToDo(""); //초기화
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={toDo}
+        onChange={onChange}
+        type="text"
+        placeholder="Write your to do"
+      ></input>
+      <button onClick={onSubmit}>Add To Do</button>
     </div>
   );
 }
