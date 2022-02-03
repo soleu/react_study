c75c3085d6a3a7deaa4d833810e284577d795c11
soleu <dlthf555@gmail.com>
2022-02-04 01:44:24 +0900

    add : movie app

---
+++ movie-app/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+// BrowserRouter - > 일반적인 홈페이지 경로
+// HashRouter -> 앞에 /#/가 붙음
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
